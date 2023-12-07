1e3041f5707dbaa3e95c5df856114b68918a68d7
Im-harshtiwari <harshtiwari20200@gmail.com>
2023-12-08 02:20:54 +0530

    Adding Day1 topic

---
+++ Day 1 JS/Declare-using-var.js
@@ -0,0 +1,9 @@
+//In js the variable can be defined or declare by using VAR word..
+var a = "Harsh Tiwari"
+var b = "Im from Lucknow"
+
+console.log(a + b)
+
+//the space used in between a & b is used to give the space between this two variable 
+//output is Harsh Tiwari I'm from Lucknow
+//without adding space it'll looks like Harsh TiwariI;m from Lucknow
---
+++ Day 1 JS/helloworld.js
@@ -0,0 +1,2 @@
+console.log("Hello World from JS")
+//This is my first JS program i'm going to make hands on JS will be better and starting from scratch to advance
