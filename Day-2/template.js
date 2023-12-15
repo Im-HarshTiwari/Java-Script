c6cf0fca5cfc697fb3151193f5a18919eaeb0eab
Harsh Tiwari <92631120+Im-HarshTiwari@users.noreply.github.com>
2023-12-15 20:03:13 +0530

    Update template.js
    update the template js

--- Day-2/template.js
+++ Day-2/template.js
@@ -1,4 +1,5 @@
 //templaye is used to print the statement along with variable
+//it can be usefull if we want to print statement 
 let name = "Harsh";
 let age = 20;
 let aboutme = 'i am ${name} and ${age}';
